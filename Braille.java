10a02ee3c35ab50c74e54e2df4e5cd245a3c815e
Gungnir44 <45889951+Gungnir44@users.noreply.github.com>
2023-10-02 13:05:07 -0500

    Add files via upload
    Few updates to this one, still need it completed by Dylan and Izaiah please.

--- Braille.java
+++ Braille.java
@@ -4,12 +4,19 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.SystemClock;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class VoiceRecognition {
     private Context context;
@@ -116,5 +123,231 @@ public class VoiceRecognition {
             textToSpeech.stop();
             textToSpeech.shutdown();
         }
+    }
+}
+
+public class CommandProcessor {
+
+    public static void processCommand(String command) {
+        if (command.contains("call")) {
+            if (command.contains("make")) {
+                makePhoneCall(command);
+            } else if (command.contains("reject")) {
+                rejectPhoneCall();
+            } else if (command.contains("answer")) {
+                answerPhoneCall();
+            }
+        } else if (command.contains("text")) {
+            if (command.contains("read text message")) {
+                String[] messages = readTextMessages();
+                if (messages != null && messages.length > 0) {
+                    speakResponse("Here are your text messages:");
+                    for (String message : messages) {
+                        speakResponse(message);
+                    }
+                } else {
+                    speakResponse("You have no text messages.");
+                }
+            } else if (command.contains("send text message")) {
+                // Extract recipient and message from the command
+                String to = "recipient@example.com"; // Replace with actual recipient
+                String message = "This is a test message.";
+                sendTextMessage(to, message);
+                speakResponse("Message sent.");
+            }
+        } else if (command.contains("read emails")) {
+            String[] emails = readEmails(); // Replace with actual email reading logic
+            if (emails != null && emails.length > 0) {
+                speakResponse("Here are your emails: " + String.join(", ", emails));
+            } else {
+                speakResponse("You have no emails.");
+            }
+        } else if (command.contains("compose email")) {
+            // Extract recipient, subject, and message from the command
+            String to = "recipient@example.com"; // Replace with actual recipient
+            String subject = "Test Subject";
+            String message = "This is a test email.";
+            composeEmail(to, subject, message); // Replace with actual email composition logic
+            speakResponse("Email sent.");
+        } else if (command.contains("search YouTube")) {
+            // Extract the search query from the command
+            String query = command.substring(command.indexOf("search YouTube for") + "search YouTube for".length()).trim();
+            searchYouTube(query);
+        } else if (command.contains("time")) {
+            String time = getCurrentTime();
+            speakResponse("The current time is " + time);
+        } else if (command.contains("date")) {
+            String date = getCurrentDate();
+            speakResponse("Today's date is " + date);
+        } else if (command.contains("Commands")) {
+            // List command prompts for the user as a reminder of capabilities
+            listCommands();
+        } else if (command.contains("set a timer")) {
+            // Extract the time duration from the command (e.g., "set a timer for 5 minutes")
+            int duration = extractDuration(command);
+            if (duration > 0) {
+                setTimer(duration);
+            }
+        } else if (command.contains("set an alarm for")) {
+            // Extract the alarm time from the command (e.g., "set an alarm for 8:30 AM")
+            int[] alarmTime = extractAlarmTime(command);
+            if (alarmTime != null) {
+                setAlarm(alarmTime[0], alarmTime[1]);
+            }
+        } else if (command.contains("search web for")) {
+            // Extract the search query from the command
+            String query = command.substring(command.indexOf("search for") + "search for".length()).trim();
+            webSearch(query);
+        } else if (command.contains("calculator")) {
+            // Extract the calculation command from the voice input (e.g., "calculate 5 plus 3")
+            String calculationCommand = command.substring(command.indexOf("calculate") + "calculate".length()).trim();
+            performCalculation(calculationCommand);
+        } else if (command.contains("create calendar event")) {
+            // Extract the event name from the voice input
+            String event = command.substring(command.indexOf("create calendar event") + "create calendar event".length()).trim();
+            createCalendarEvent(event);
+        } else {
+            Log.d("CommandProcessor", "Command not recognized");
+        }
+    }
+
+    // Define the corresponding methods for each functionality (makePhoneCall, rejectPhoneCall, etc.)
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Replace with your layout file
+    }
+
+    // Function to make a phone call
+    private void makePhoneCall(String phoneNumber) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+        startActivity(callIntent);
+    }
+
+    // Function to reject a phone call
+    private void rejectPhoneCall() {
+        try {
+            Process process = Runtime.getRuntime().exec("input keyevent 26");
+            process.waitFor();
+            SystemClock.sleep(1000); // Wait for the screen to turn off
+            process = Runtime.getRuntime().exec("input keyevent 6");
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Function to answer a phone call
+    private void answerPhoneCall() {
+        try {
+            Process process = Runtime.getRuntime().exec("input keyevent 79");
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Function to read text messages
+    private String[] readTextMessages() {
+        try {
+            Process process = Runtime.getRuntime().exec("content query --uri content://sms/inbox");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder output = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            process.waitFor();
+            return output.toString().split("\n");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Function to send a text message
+    private void sendTextMessage(String phoneNumber, String message) {
+        Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
+        sendIntent.setData(Uri.parse("smsto:" + phoneNumber));
+        sendIntent.putExtra("sms_body", message);
+        startActivity(sendIntent);
+    }
+
+    // Function to authenticate with the Gmail API
+    // You need to implement this based on the Gmail API documentation
+    // For Gmail API authentication, you can use libraries like Google Sign-In
+    private void authenticateWithGmailAPI() {
+        // Implement Gmail API authentication logic here
+    }
+
+    // Function to read emails
+    // You need to implement this based on the Gmail API documentation
+    private String[] readEmails() {
+        // Implement email reading logic here
+        return null;
+    }
+
+    // Function to compose an email
+    // You need to implement this based on the Gmail API documentation
+    private void composeEmail(String to, String subject, String message) {
+        // Implement email composition logic here
+    }
+
+    // Function to set a timer
+    // You need to implement timer functionality based on Android's timer mechanisms
+    private void setTimer(int seconds) {
+        // Implement timer logic here
+    }
+
+    // Function to set an alarm
+    // You need to implement alarm functionality based on Android's alarm mechanisms
+    private void setAlarm(int hour, int minute) {
+        // Implement alarm logic here
+    }
+
+    // Function to perform a web search
+    // You need to implement web search functionality based on Android's WebView or external browser
+    private void webSearch(String query) {
+        // Implement web search logic here
+    }
+
+    // Function to search YouTube
+    // You need to implement YouTube search functionality based on YouTube API or WebView
+    private void searchYouTube(String query) {
+        // Implement YouTube search logic here
+    }
+
+    // Function to perform a calculation
+    private void performCalculation(String command) {
+        try {
+            // Replace words with corresponding symbols
+            command = command.replace("plus", "+").replace("minus", "-").replace("times", "*").replace("divided by", "/");
+
+            // Evaluate the mathematical expression
+            double result = evaluateMathExpression(command);
+            String response = "The result is " + result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            String response = "An error occurred during the calculation.";
+        }
+    }
+
+    // Function to evaluate a mathematical expression
+    private double evaluateMathExpression(String expression) {
+        // Implement mathematical expression evaluation logic here
+        // You can use a library like javax.script.ScriptEngine for evaluation
+        return 0.0; // Replace with the actual result
+    }
+
+    // Function to create a calendar event
+    // You need to implement calendar event creation based on Android's calendar mechanisms
+    private void createCalendarEvent(String eventName) {
+        // Implement calendar event creation logic here
+    }
+
+    // Function to speak a response
+    private void speakResponse(String response) {
+        // Implement text-to-speech functionality to speak the response
     }
 }
